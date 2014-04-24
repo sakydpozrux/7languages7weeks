0e84319935962e2d981c4ba227231e9e34adc77e
Szymon Koper <sakydpozrux@gmail.com>
2014-04-24 20:37:04 +0200

    I consider 1st day of Prolog as done.

---
+++ prolog/day1.pl
@@ -0,0 +1,27 @@
+
+book(the_colour_of_magic).
+book(thief_of_time).
+book(carpe_jugulum).
+book(small_gods).
+book(seven_languages_seven_weeks).
+book(symfonia_cpp).
+book(the_old_man_and_the_see).
+book(forrest_gump).
+book(gump_and_co).
+
+book_author(the_colour_of_magic, terry_pratchett).
+book_author(thief_of_time, terry_pratchett).
+book_author(carpe_jugulum, terry_pratchett).
+book_author(small_gods, terry_pratchett).
+book_author(seven_languages_seven_weeks, bruce_tate).
+book_author(symfonia_cpp, jerzy_grebosz).
+book_author(the_old_man_and_the_see, ernest_hemingway).
+book_author(forrest_gump, winston_groom).
+book_author(gump_and_co, winston_groom).
+
+
+% book_author(X, terry_pratchett).
+
+% I'm lazy and I won't do excercise about musicians, instruments and music genres.
+
+
