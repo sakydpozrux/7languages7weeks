bea60f9a417e31f9806216bec858617951382797
Szymon Koper <sakydpozrux@gmail.com>
2014-04-25 01:19:10 +0200

    Finding minimal element on the list

--- prolog/day2.pl
+++ prolog/day2.pl
@@ -4,3 +4,13 @@ reverse([S|LTail], R) :-
   reverse(LTail, RWithoutLast),
   append(RWithoutLast, [S], R).
 
+
+list_min(A, [A]) :- !.
+list_min(A, [X, Xs|Xss]) :-
+  X =< Xs,
+  list_min(A, [X|Xss]), !.
+list_min(A, [X, Xs|Xss]) :-
+  X > Xs,
+  list_min(A, [Xs|Xss]).
+
+
